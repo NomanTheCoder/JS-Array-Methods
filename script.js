563c8f3d3234544e4d00f3fce0c67c4f87d82cc5
Kayani815 <nomannadeem815@gmail.com>
2024-03-09 17:44:02 +0500

    JS Array methods

---
+++ script.js
@@ -0,0 +1,35 @@
+const cars=['Toyota','Honda','Prado','Mehran','Suzuki'];
+console.log(cars.length);//length of array
+
+const city=['Rawalpindi','Multan','Peshawar','Islamabad','Lahore','Karachi'];
+console.log(city.toString()); //Tostring() method to seperate values in Comma,
+
+const name=['Usman','Ahmed','Furqan','Ahsan','Hassan'];
+console.log(name.at(2)); //Find Value by Giving Index Number at() with parentheses
+
+const Books=['Physics','Urdu','Chemistry','Biology','Computer Science'];
+console.log(Books[3]);//Find Value by Giving Index Number [] with Square Brackets
+
+const fruits = ["Strawberry", "Orange", "Apple", "Mango",'Banana'];
+console.log(fruits.join('#'));
+
+const matchs=['Islmabad','Peshawar','Lahore','Karachi','Multan'];
+console.log(matchs.pop());// Delete the last Element of array by Pop().
+
+const match=['Islmabad','Peshawar','Lahore','Karachi','Multan'];
+console.log(match.push('Quetta'));//Add New Element in Array By Push().
+console.log(match);
+
+const country=['Pakistan','Oman','Saudi Arabia','Qatar'];
+console.log(country.shift()); //Shift method Delete First Element of array and move array to left side
+console.log(country);
+console.log(country.unshift('Pakistan')); //Recover the Delete Element By Unshift() Method.
+console.log(country);
+
+
+
+
+
+
+
+
